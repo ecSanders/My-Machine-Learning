ae9a1def76d3643c612a25fcb0d1652f5722894b
ecSanders <ecsander0@gmail.com>
2021-10-05 00:30:14 -0600

    Constructors and Methods

--- DataFrame.cpp
+++ DataFrame.cpp
@@ -1,10 +1,27 @@
 #include "DataFrame.h"
 
-DataFrame::DataFrame(/* args */)
-{
+
+
+const Series DataFrame::getColumn(std::vector<Series> dfData, int index){
+    return this->dfData.at(index);
+}; 
+
+void DataFrame::addColumn(std::vector<Series> dfData, Series newData){
+    this->dfData.push_back(newData);
+};
+
+
+
+
+DataFrame::DataFrame(){
+
 }
 
-DataFrame::~DataFrame()
-{
+DataFrame::DataFrame(std::vector<Series> dfData){
+    this->dfData = dfData;
+}
+
+DataFrame::~DataFrame(){
+
 }
 
--- DataFrame.h
+++ DataFrame.h
@@ -1,17 +1,25 @@
-#include <vector>
+#include "Series.h"
 
 
-class DataFrame: private Series
+class DataFrame: public Series
 
 {
 private:
-    DataFrame(std::vector<Series series>);
+    DataFrame();
+    DataFrame(std::vector<Series> dfData);
+
     ~DataFrame();
+
+    const Series getColumn(std::vector<Series> dfData, int index); 
+    
+    void addColumn(std::vector<Series> dfData, Series newData);
+    void deleteColumn(std::vector<Series> dfData, int index);
 
 
 
 public:
     Series series;
+    std::vector<Series> dfData;
 };
 
 
