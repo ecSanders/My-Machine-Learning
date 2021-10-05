849e43eb55ed275069dd88448e6fe96a8d3fc489
ecSanders <ecsander0@gmail.com>
2021-10-05 00:31:14 -0600

    A substitute class so I dont have errors

---
+++ Series.cpp
@@ -0,0 +1,10 @@
+#include "Series.h"
+
+const std::vector<int> Series::setData(std::vector<int> data){
+    this-> data = data;
+    return data;
+}
+
+std::vector<int> Series::getData(std::vector<int> data){
+    return data;
+}
